135cdbd02a8a1e72fc0e61919861429f29978ad8
Logan Lopez <lizardlab@yahoo.com>
2025-12-02 15:56:08 -0500

    day2 part2 finished

--- 25/src/bin/day2.rs
+++ 25/src/bin/day2.rs
@@ -1,4 +1,5 @@
 use std::{env, fs};
+use fancy_regex::Regex;
 fn main() {
     let args: Vec<String>  = env::args().collect();
     if args.len() != 2{
@@ -7,12 +8,14 @@ fn main() {
     let filename = args.get(1).unwrap();
     let barcodes = fs::read_to_string(filename).expect("File error");
     let ranges: Vec<_> = barcodes.trim().split(',').collect();
-    let mut count:i64 = 0;
+    let mut part1:i64 = 0;
+    let mut part2:i64 = 0;
+    let repeat_match = Regex::new(r"^(.+)(?:\1)+$").unwrap();
     for rng in ranges.iter() {
         let ends: Vec<_> = rng.split('-').collect();
         if ends[1] != ""{
             let start = ends[0].parse::<i64>().unwrap();
-            let end = ends[1].parse::<i64>().expect("asdf");
+            let end = ends[1].parse::<i64>().unwrap();
             //let diff = end - start;
             for i in start..end+1{
                 let id = i.to_string();
@@ -20,11 +23,15 @@ fn main() {
                     let first = id.get(0..id.len()/2).unwrap();
                     let last = id.get(id.len()/2..).unwrap();
                     if first == last{
-                        count += i;
+                        part1 += i;
                     }
+                }
+                if repeat_match.is_match(&id).unwrap(){
+                    part2 += i
                 }
             }
         }
     }
-    println!("Part 1: {count:?}");
+    println!("Part 1: {part1:?}");
+    println!("Part 2: {part2:?}");
 }
