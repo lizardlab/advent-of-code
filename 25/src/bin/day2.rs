239f578e2b0694136744f7b5169a6ab01e563aff
Logan Lopez <lizardlab@yahoo.com>
2025-12-02 14:59:15 -0500

    day2 mostly working

---
+++ 25/src/bin/day2.rs
@@ -0,0 +1,31 @@
+use std::{env, fs};
+fn main() {
+    let args: Vec<String>  = env::args().collect();
+    if args.len() != 2{
+        eprintln!("Provide ranges");
+    }
+    let filename = args.get(1).unwrap();
+    let barcodes = fs::read_to_string(filename).expect("File error");
+    let ranges: Vec<_> = barcodes.split(',').collect();
+    let mut count:i64 = 0;
+    for rng in ranges.iter() {
+        let ends: Vec<_> = rng.split('-').collect();
+        if ends[1] != ""{
+            let start = ends[0].parse::<i64>().unwrap();
+            println!("{}", ends[1]);
+            let end = ends[1].parse::<i64>().expect("Ends failed");
+            //let diff = end - start;
+            for i in start..end+1{
+                let id = i.to_string();
+                if id.len() % 2 == 0{
+                    let first = id.get(0..id.len()/2).unwrap();
+                    let last = id.get(id.len()/2..).unwrap();
+                    if first == last{
+                        count += i;
+                        println!("{}", count);
+                    }
+                }
+            }
+        }
+    }
+}
