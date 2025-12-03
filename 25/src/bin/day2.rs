ddcfe2d5c0e1fdecd88ce1dbaca1d75d2eb2b166
Logan Lopez <lizardlab@yahoo.com>
2025-12-03 17:25:11 -0500

    start day3

--- 25/src/bin/day2.rs
+++ 25/src/bin/day2.rs
@@ -13,22 +13,20 @@ fn main() {
     let repeat_match = Regex::new(r"^(.+)(?:\1)+$").unwrap();
     for rng in ranges.iter() {
         let ends: Vec<_> = rng.split('-').collect();
-        if ends[1] != ""{
-            let start = ends[0].parse::<i64>().unwrap();
-            let end = ends[1].parse::<i64>().unwrap();
-            //let diff = end - start;
-            for i in start..end+1{
-                let id = i.to_string();
-                if id.len() % 2 == 0{
-                    let first = id.get(0..id.len()/2).unwrap();
-                    let last = id.get(id.len()/2..).unwrap();
-                    if first == last{
-                        part1 += i;
-                    }
+        let start = ends[0].parse::<i64>().unwrap();
+        let end = ends[1].parse::<i64>().unwrap();
+        //let diff = end - start;
+        for i in start..end+1{
+            let id = i.to_string();
+            if id.len() % 2 == 0{
+                let first = id.get(0..id.len()/2).unwrap();
+                let last = id.get(id.len()/2..).unwrap();
+                if first == last{
+                    part1 += i;
                 }
-                if repeat_match.is_match(&id).unwrap(){
-                    part2 += i
-                }
+            }
+            if repeat_match.is_match(&id).unwrap(){
+                part2 += i
             }
         }
     }
